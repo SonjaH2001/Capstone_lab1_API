2a8070c74cc16c2669ca8d2d76918afe70b190f7
SH <email@email.com>
2017-10-06 12:01:32 -0500

    added error handling

--- Main_file.py
+++ Main_file.py
@@ -4,27 +4,34 @@
 import os #clicked on os when it was red, it imports for you!!
 
 import requests #ignore red squiggle
-from urllib.parse import quote #handles the q= in the API string
+from urllib.parse import quote #handles the q= in the API string/ sanitize the odd characters in the URL%<&
 import json
 key = os.environ.get("key") # I put the value into the evironment variable and named it key
 
 def get_temp(city, country):
     resp = requests.get('http://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=%s' \
                  % (quote(city), quote(country), key))
-    parsed_json = resp.json()
-    tempInKelvin = parsed_json['main']['temp']
-    return tempInKelvin
+    if resp.status_code == 200:
+        parsed_json = resp.json() # gets the json from the resp. parses json from response body aka content
+        tempInKelvin = parsed_json['main']['temp']  #json in python is a dictionary, main is a dictionary of stuff.
+        return tempInKelvin
+    else:
+        return None
 
 def main():
 
-    city = input("Enter the city to search for")
-    country = input("What country is that in?")
+    city = input("Enter the city to search for: ")
+    country = input("What country is that in? ")
+    is_valid(city, country) #data validation function
     temp = get_temp(city, country)
     if temp is not None:
-        print('The temperature in %s %s is %f degrees C ' % (city, country, temp))
+        print('The temperature in %s %s is %f degrees K ' % (city, country, temp))
     else :
         print('Unable to get the temperature.')
 
 #need to do error handling, cache locally, etc
+def is_valid(city, country):
+    if city == "":
+        print("enter the city or country dammit ")
 
 main()
