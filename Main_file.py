320f9d476d8a1fbc465819c914801284afdc12a3
SH <email@email.com>
2017-10-10 13:57:43 -0500

    added 401 error handling

--- Main_file.py
+++ Main_file.py
@@ -7,7 +7,7 @@ import requests #ignore red squiggle
 from urllib.parse import quote #handles the q= in the API string/ sanitize the odd characters in the URL%<&
 import json
 key = os.environ.get("key") # I put the value into the evironment variable and named it key
-
+# key = "bananas"
 def get_temp(city, country):
     resp = requests.get('http://api.openweathermap.org/data/2.5/weather?q=%s,%s&APPID=%s' \
                  % (quote(city), quote(country), key))
@@ -15,6 +15,9 @@ def get_temp(city, country):
         parsed_json = resp.json() # gets the json from the resp. parses json from response body aka content
         tempInKelvin = parsed_json['main']['temp']  #json in python is a dictionary, main is a dictionary of stuff.
         return tempInKelvin
+    if resp.status_code == 401:
+        print("Access denied, invalid API key")
+        #add more of these!!!
     else:
         return None
 
@@ -29,10 +32,12 @@ def main():
         clesius = convert_temp(temp)
         # print('The temperature in %s %s is %f degrees celsius' % (city, country, temp))
         print("Celsius is ", clesius )
+        #addd farenheit
     else :
         print('Unable to get the temperature.')
 
 #need to do error handling, cache locally, etc
+# You should handle errors, such as access denied, no network connection, invalid response....
 def is_valid(city, country):
     if city == "":
         print("enter the city or country dammit ")
